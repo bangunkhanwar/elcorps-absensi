c35a31af2d6d3f435b9a3df0ae7343bd8e403b42
bangunkhanwar <bangunkhanwar@gmail.com>
2026-01-19 15:22:02 +0700

    update untuk build

--- website/src/screens/ShiftCalendar.tsx
+++ website/src/screens/ShiftCalendar.tsx
@@ -48,12 +48,12 @@ const ShiftCalendar: React.FC<ShiftCalendarProps> = ({
   employeeShifts,
   searchData,
   setSearchData,
-  selectedUnit,
+  selectedUnit: _selectedUnit,
   selectedUnitName,
   currentPage,
   setCurrentPage,
   itemsPerPage,
-  loading = false
+  loading: _loading = false
 }) => {
   const [selectedDate, setSelectedDate] = useState<string>(new Date().toISOString().split('T')[0])
 
@@ -262,7 +262,7 @@ const ShiftCalendar: React.FC<ShiftCalendarProps> = ({
                               )}
                             </div>
                           </td>
-                          {weekDates.map((date, index) => (
+                          {weekDates.map((_date, index) => (
                             <td key={index} className="px-3 sm:px-4 py-4 text-center min-w-[110px] sm:min-w-32">
                               {employeeShifts[employee.id] ? (
                                 <div className="flex flex-col items-center">
